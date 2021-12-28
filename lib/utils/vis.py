f4ac2e1ef63e08fc51733cdf042b607b41f59261
Shenhan Qian <qianshh@shanghaitech.edu.cn>
2021-12-28 14:54:22 +0800

    Fixed a bug in visualization.

--- lib/utils/vis.py
+++ lib/utils/vis.py
@@ -44,10 +44,10 @@ def save_batch_image_with_joints(batch_image, batch_joints, batch_joints_vis,
             joints_vis = batch_joints_vis[k]
 
             for joint, joint_vis in zip(joints, joints_vis):
-                joint[0] = x * width + padding + joint[0]
-                joint[1] = y * height + padding + joint[1]
+                joint_x = x * width + padding + joint[0]
+                joint_y = y * height + padding + joint[1]
                 if joint_vis[0]:
-                    cv2.circle(ndarr, (int(joint[0]), int(joint[1])), 2, [255, 0, 0], 2)
+                    cv2.circle(ndarr, (int(joint_x), int(joint_y)), 2, [255, 0, 0], 2)
             k = k + 1
     cv2.imwrite(file_name, ndarr)
 
@@ -80,18 +80,18 @@ def save_batch_image_with_joints_gt_pred(batch_image,
             joints = batch_joints_gt[k]
             joints_vis = batch_joints_vis_gt[k]
             for joint, joint_vis in zip(joints, joints_vis):
-                joint[0] = x * width + padding + joint[0]
-                joint[1] = y * height + padding + joint[1]
+                joint_x = x * width + padding + joint[0]
+                joint_y = y * height + padding + joint[1]
                 if joint_vis[0]:
-                    cv2.circle(ndarr, (int(joint[0]), int(joint[1])), 2, [0, 255, 0], 2)
+                    cv2.circle(ndarr, (int(joint_x), int(joint_y)), 2, [0, 255, 0], 2)
             
             joints = batch_joints_pred[k]
             joints_vis = batch_joints_vis_pred[k]
             for joint, joint_vis in zip(joints, joints_vis):
-                joint[0] = x * width + padding + joint[0]
-                joint[1] = y * height + padding + joint[1]
+                joint_x = x * width + padding + joint[0]
+                joint_y = y * height + padding + joint[1]
                 if joint_vis[0]:
-                    cv2.circle(ndarr, (int(joint[0]), int(joint[1])), 2, [255, 0, 0], 2)
+                    cv2.circle(ndarr, (int(joint_x), int(joint_y)), 2, [255, 0, 0], 2)
 
             k = k + 1
     cv2.imwrite(file_name, ndarr)
